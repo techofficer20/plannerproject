0a68c0c012ec817cf449bcc886c2679e5f16c0c2
jungss98 <sjj98@naver.com>
2019-07-13 16:37:17 +0900

    cal-ver1

---
+++ plannerproject/cal/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from cal import views
+
+app_name = 'cal'
+urlpatterns = [
+    url(r'^index/$', views.index, name='index'),
+    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'), # here
+]
--- plannerproject/plannerproject/settings.py
+++ plannerproject/plannerproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'plannerapp.apps.PlannerappConfig',
-    'accountsapp.apps.AccountsappConfig'
+    'accountsapp.apps.AccountsappConfig',
+    'cal.apps.CalConfig'
 ]
 
 MIDDLEWARE = [
