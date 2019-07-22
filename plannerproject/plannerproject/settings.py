0c1f9ce2caad6a9bd21b5ec01059d23d24d31e32
jungss98 <sjj98@naver.com>
2019-07-23 00:26:35 +0900

    calendar fin

--- plannerproject/plannerproject/settings.py
+++ plannerproject/plannerproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'plannerapp.apps.PlannerappConfig',
     'accountsapp.apps.AccountsappConfig',
-    'cal'
+    'cal.apps.CalConfig',
     
 ]
 
